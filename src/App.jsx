48b2cdccbf1a020085818708b9134a64903e7733
Ankur Das <132288002+Ankur6580@users.noreply.github.com>
2024-06-10 14:30:11 +0530

    Uploaded all the files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1 className="h-1/5 w-full text-red-700 font-black text-4xl bg-green-400 p-4 text-center">
+        Apple Slicer
+      </h1>
+    </>
+  );
+}
+
+export default Header;
